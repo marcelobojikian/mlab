cb43e190c8b9ef88c93825c65d6db43dc60369d7
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-10 03:05:14 +0000

    Update variable

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ canonical() {
 PROJECT_ROOT=https://github.com/marcelobojikian/mlab/raw/main
 
 COMMAND_NAME=mlab
-COMMAND_CONF=$(canonical "~/.$COMMAND_NAME")
+COMMAND_PATH_CONF=$(canonical "~/.$COMMAND_NAME")
 
 echo Instalando Home Lab library
 
-echo Set default configuration on path $COMMAND_CONF/conf.txt
-mkdir -p "$COMMAND_CONF"
-cat <<EOF > $COMMAND_CONF/conf.txt
+echo Config Path: "$COMMAND_PATH_CONF"
+mkdir -p "$COMMAND_PATH_CONF"
+
+echo Set default configuration on path $COMMAND_PATH_CONF/conf.txt
+cat <<EOF > $COMMAND_PATH_CONF/conf.txt
 URI=$PROJECT_ROOT/src
 LOG_LEVEL=ERROR
 EOF
@@ -23,7 +25,7 @@ echo Download command $COMMAND_NAME from $PROJECT_ROOT
 TO=/usr/local/sbin/$COMMAND_NAME
 sudo curl -sSLo "$TO" --progress-bar "$PROJECT_ROOT/src/proxmox/cmd.sh" && sudo chmod +x "$TO"
 
-echo Enable cache on path $COMMAND_CONF/cache
-$COMMAND_NAME cache enable --url="$PROJECT_ROOT/src" --path="$COMMAND_CONF/cache"
+echo Enable cache on path $COMMAND_PATH_CONF/cache
+$COMMAND_NAME cache enable --url="$PROJECT_ROOT/src" --path="$COMMAND_PATH_CONF/cache"
 
 echo Instalacao completa
