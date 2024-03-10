786177ff8889c72dade85426d5f90432858312f5
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-03-10 00:54:40 +0000

    Fixed installer

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-echo Instalando Home Lab lib
+PROJECT_ROOT=https://github.com/marcelobojikian/mlab/raw/main
 
-URI=https://raw.githubusercontent.com/marcelobojikian/mlab/main/src/proxmox
-TO=/usr/local/sbin/mlab
+COMMAND_NAME=mlab
+COMMAND_CONF=~/.$COMMAND_NAME
 
-sudo wget "$URI/cmd.sh" -O "$TO" && sudo chmod +x "$TO"
+echo Instalando Home Lab library
+
+echo Set default configuration on path $COMMAND_CONF/config.txt
+cat <<EOF > $COMMAND_CONF/conf.txt
+URI=$PROJECT_ROOT/src
+LOG_LEVEL=ERROR
+EOF
+
+echo Download command $COMMAND_NAME from $PROJECT_ROOT
+
+TO=/usr/local/sbin/$COMMAND_NAME
+sudo curl -sSLo "$TO" --progress-bar "$PROJECT_ROOT/src/proxmox/cmd.sh" && sudo chmod +x "$TO"
+
+echo Enable cache on path $COMMAND_CONF/cache
+$COMMAND_NAME cache enable --url="$PROJECT_ROOT/src" --path="$COMMAND_CONF/cache"
 
 echo Instalacao completa
